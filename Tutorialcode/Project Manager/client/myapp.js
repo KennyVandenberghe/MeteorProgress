bb1fa553c5896561b79edde7167a53151836622a
Kenny Vandenberghe <kenny.vandenberghe@commercieelwebverkeer.be>
2014-02-04 10:04:35 +0100

    Project Manager toegevoegd en status update 2014-02-03

---
+++ Tutorialcode/Project Manager/client/myapp.js
@@ -0,0 +1,102 @@
+Projects = new Meteor.Collection('projects');
+if (Meteor.isClient) {
+    Session.setDefault('appName', 'Project Manager');
+    Session.setDefault('showProjectDialog', false);
+    Session.setDefault('editing_project', null);
+    Meteor.Router.add({
+        '/': 'homepage',
+        '/projects': 'projects',
+        '/employees': 'employees'
+    })
+    Handlebars.registerHelper("formatDate", function(datetime) {
+        if (moment) {
+            return moment(datetime).format("MM/DD/YYYY");
+        } else {
+            return datetime;
+        }
+    })
+    Template.menu.appName = function() {
+        return Session.get('appName');
+    }
+    Template.projects.projectList = function() {
+        return Projects.find();
+    }
+    Template.projectForm.events({
+        'click .save': function(evt, tmpl) {
+            var name = tmpl.find('.name').value;
+            var client = tmpl.find('.client').value;
+            var status = tmpl.find('.status').value;
+            if (Session.get('editing_project')) {
+                updateProject(name, client, status);
+            } else {
+                addProject(name, client, status);
+            }
+            Session.set('showProjectDialog', false);
+            Session.set("editing_project", null);
+
+        },
+        'click .cancel': function(evt, tmpl) {
+            Session.set('showProjectDialog', false);
+            Session.set("editing_project", null);
+
+        },
+        'click .remove': function(evt, tmpl) {
+            removeProject();
+            Session.set('showProjectDialog', false);
+            Session.set("editing_project", null);
+
+        }
+    })
+    Template.projects.events({
+        'click .addProject': function(evt, tmpl) {
+            Session.set('showProjectDialog', true);
+        }
+    })
+    Template.projectForm.rendered = function() {
+        var project = Projects.findOne({
+            _id: Session.get('editing_project')
+        });
+        $('.status').val(project.status);
+    };
+    Template.projectRow.events({
+        'dblclick .projectRow': function(evt, tmpl) {
+            Session.set("editing_project", tmpl.data._id);
+            Session.set('showProjectDialog', true);
+        }
+    })
+    Template.projectForm.project = function() {
+        return Projects.findOne({
+            _id: Session.get('editing_project')
+        });
+    }
+    Template.projectForm.editing_project = function() {
+        return Session.get('editing_project');
+    }
+    Template.projects.showProjectDialog = function() {
+        return Session.get('showProjectDialog');
+    }
+    Template.projects.editing_project = function() {
+        return Session.get('editing_project');
+    }
+    var addProject = function(name, client, status) {
+        Projects.insert({
+            name: name,
+            client: client,
+            status: status
+        });
+    }
+    var updateProject = function(name, client, status) {
+        Projects.update(Session.get('editing_project'), {
+            $set: {
+                name: name,
+                client: client,
+                status: status
+            }
+        });
+    }
+    var removeProject = function() {
+        Projects.remove({
+            _id: Session.get('editing_project')
+        });
+    }
+}
---
+++ Tutorialcode/Project Manager/server/server.js
@@ -0,0 +1,11 @@
+if (Meteor.isServer) {
+    Projects = new Meteor.Collection('projects');
+
+    Meteor.startup(function() {
+        Meteor.methods({
+            removeAllProjects: function() {
+                Projects.remove({})
+            }
+        })
+    });
+}
